94ebb5e8c141523ed464f30a63054bdd07e28838
MonaIA1 <51651943+MonaIA1@users.noreply.github.com>
2024-01-11 22:34:44 +0000

    Update losses.py

--- model/losses.py
+++ model/losses.py
@@ -10,7 +10,7 @@ class WCE_k3Clusters(nn.Module): # combine random sampling and balanced weights
     def __init__(self,device, **kwargs):
         super(WCE_DBSCANClusters, self).__init__()
         # class weights, used k-means k=3
-        #empty = 1 random sampling, ciel = 3 , floor = 2, wall = 2, window = 3, chair = 3, bed =3, sofa = 3, table= 3, tvs = 3, furn = 1, obj = 2
+        #occl_empty = 1, ciel = 3 , floor = 2, wall = 2, window = 3, chair = 3, bed =3, sofa = 3, table= 3, tvs = 3, furn = 1, obj = 2
         class_weights = torch.tensor([1.0, 3.0, 2.0, 2.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 1.0, 2.0]).to(device)
         self.loss_fun = nn.CrossEntropyLoss(weight=class_weights)
 
