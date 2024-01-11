dc2f8331e3ff5c878b3aba24bfe43641625b52fd
MonaIA1 <51651943+MonaIA1@users.noreply.github.com>
2024-01-11 22:40:04 +0000

    Update run_t_job.sh

--- run_t_job.sh
+++ run_t_job.sh
@@ -4,6 +4,6 @@
 #SBATCH --gres=gpu:3
 #SBATCH --mem=100GB
 #SBATCH --time=60:00:00          # walltime
-export CUDA_VISIBLE_DEVICES=0,1,2
+export CUDA_VISIBLE_DEVICES=0
 
-python train.py --dataset NYU --model_name ResUNet --expr_name WBCE --train_batch_size 4 --val_batch_size 2 --base_lr 0.01 --decay 0.0005 --epochs 60
+python train.py --dataset NYU --model_name ResUNet --expr_name clust_k3 --train_batch_size 4 --val_batch_size 2 --base_lr 0.01 --decay 0.0005 --epochs 100
