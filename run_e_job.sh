5c55d052da1e1f2a0d6842887e3f82674bde4beb
MonaIA1 <51651943+MonaIA1@users.noreply.github.com>
2024-01-12 14:32:43 +0000

    Update run_e_job.sh

--- run_e_job.sh
+++ run_e_job.sh
@@ -4,4 +4,4 @@
 #SBATCH --gres=gpu:1
 #SBATCH --time=00:30:00          # walltime
 
-python evaluation.py --dataset NYUCAD --model_name ResUNet --expr_name 3NYUCAD_WCE_BalancedClusters_60 --weights "./saved_models/ResUNet_3NYUCAD_WCE_BalancedClusters_60_fold1_2023-08-21.pth" "./saved_models/ResUNet_3NYUCAD_WCE_BalancedClusters_60_fold2_2023-08-21.pth" "./saved_models/ResUNet_3NYUCAD_WCE_BalancedClusters_60_fold3_2023-08-21.pth" 
+python evaluation.py --dataset NYUCAD --model_name ResUNet --expr_name k3_depthOnly_p15 --weights "./saved_models/ResUNet_k3_depthOnly_p15_fold1_2023-12-01.pth" "./saved_models/ResUNet_k3_depthOnly_p15_fold2_2023-12-02.pth" "./saved_models/ResUNet_k3_depthOnly_p15_fold3_2023-12-02.pth" 
