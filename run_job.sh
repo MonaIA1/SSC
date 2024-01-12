abecdf429c1b2aceb59cee6bd35fa94620888285
MonaIA1 <51651943+MonaIA1@users.noreply.github.com>
2024-01-12 14:22:57 +0000

    Update run_job.sh

--- run_job.sh
+++ run_job.sh
@@ -3,4 +3,4 @@
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 #SBATCH --time=00:30:00          # walltime
-python preproc_tsdf.py --base_path './data/NYUtrain' --dest_path './data/NYU_train_preproc'
+python preproc_tsdf.py --base_path './data/NYUCADtrain' --dest_path './data/NYUCAD_train_preproc'
