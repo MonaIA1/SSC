cc5cf18f844ddb646476f8b252e7130b77ac782a
MonaIA1 <51651943+MonaIA1@users.noreply.github.com>
2024-10-29 17:45:42 +0000

    Update losses.py

--- model/losses.py
+++ model/losses.py
@@ -8,7 +8,7 @@ import numpy as np
 # combine re-sampling and reqularized weightes using clustering based on k=3
 class WCE_k3Clusters(nn.Module): # combine random sampling and balanced weights
     def __init__(self,device, **kwargs):
-        super(WCE_DBSCANClusters, self).__init__()
+        super(WCE_k3Clusters, self).__init__()
         # class weights, use k-means k=3
         #occl_empty = 1, ciel = 3 , floor = 2, wall = 2, window = 3, chair = 3, bed =3, sofa = 3, table= 3, tvs = 3, furn = 1, obj = 2
         class_weights = torch.tensor([1.0, 3.0, 2.0, 2.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 1.0, 2.0]).to(device)
